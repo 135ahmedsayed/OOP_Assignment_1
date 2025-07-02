720cd6e9428723fab83496c869dc89a7822e8a93
135ahmedsayed <ahmedshkra246@gmail.com>
2025-07-03 00:28:50 +0300

    Q1 Solve

---
+++ OOP_Assignment_01/Helper.cs
@@ -0,0 +1,13 @@
+﻿namespace OOP_Assignment_01;
+#region With Q1
+public enum WeekDays
+{
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday,
+}
+#endregion
--- OOP_Assignment_01/Program.cs
+++ OOP_Assignment_01/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Q1
+            string[] value = Enum.GetNames(typeof(WeekDays));
+            Console.WriteLine("Days of the Week : ");
+            foreach (string s in value)
+                Console.WriteLine(s);
+            #endregion
+            Console.ReadKey();
         }
     }
 }
